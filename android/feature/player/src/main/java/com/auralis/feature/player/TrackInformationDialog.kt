3267f621fb65e41f7b32fe76c5a2ebd51834cb2d
zyk1172 <53221608+zyk1172@users.noreply.github.com>
2026-09-09 12:12:28 +0800

    fix(android): make track information sheet compile-safe

--- android/feature/player/src/main/java/com/auralis/feature/player/TrackInformationDialog.kt
+++ android/feature/player/src/main/java/com/auralis/feature/player/TrackInformationDialog.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.item
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
